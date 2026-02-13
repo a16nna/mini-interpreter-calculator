cd17ed8332d472001b24342dd874e2c85478959d
Anna Mariya Martin <anna.mariya_2028@woxsen.edu.in>
2026-02-13 21:30:47 +0530

    Add Flask app with webview integration

---
+++ web/app.py
@@ -0,0 +1,24 @@
+web/app.py
+import threading
+import webview
+from server import app  # import your Flask app
+
+def run_flask():
+    app.run()
+
+if __name__ == "__main__":
+    flask_thread = threading.Thread(target=run_flask)
+    flask_thread.daemon = True
+    flask_thread.start()
+
+    webview.create_window(
+    "Mini Interpreter Calculator",
+    "http://127.0.0.1:5000",
+    width=400,
+    height=600,
+    resizable=False,
+    transparent=True,
+    frameless=False
+)
+
+    webview.start()
